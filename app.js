d467497a2c93b1d1a869ebe65f2b4431b8e98b90
MakramELTimani <makram.timany@gmail.com>
2021-05-25 20:55:01 +0300

    changed the server

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var port = 3700;
 var outFile = 'demo.wav';
 var app = express();
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express);
-app.use(express.static(__dirname + '/public'))
+// app.set('views', __dirname + '/tpl');
+// app.set('view engine', 'jade');
+// app.engine('jade', require('jade').__express);
+// app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(req, res){
-  res.render('index');
-});
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
 
-app.listen(port);
+// app.listen(port);
 
-console.log('server open on port ' + port);
+// console.log('server open on port ' + port);
 
 binaryServer = BinaryServer({port: 9001});
 
